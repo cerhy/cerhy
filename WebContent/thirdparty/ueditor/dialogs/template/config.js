afe42c262feebe838f2bd52d0ecaf84c9c01fc3b
wen.xu <wen.xu@sihuatech.com>
2016-12-13 17:07:40 +0800

    cerhy

---
+++ WebContent/thirdparty/ueditor/dialogs/template/config.js
@@ -0,0 +1,89 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: xuheng
+ * Date: 12-8-8
+ * Time: 下午2:00
+ * To change this template use File | Settings | File Templates.
+ */
+var templates = [
+    {
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'文件模板',
+        "html":'<div style="padding: 6px 0px; text-align: center; border-bottom-color: #ff0000; border-bottom-width: 3px; border-bottom-style: solid;"><div style="padding: 30px 0px; color: #ff0000; font-family: 宋体; font-size: 38px; font-weight: bold;">海南省教育研究培训院文件</div><div style="text-align: center; line-height: 180%; font-family: 仿宋_GB2312, 仿宋; font-size: 18px;">琼教研训〔2016〕17号</div></div><div style="padding: 32px 0px; text-align: center; line-height: 180%; font-family: 黑体; font-size: 26px;">海南省教育研究培训院 <br />关于xxxxx的通知</div><div style="text-align: justify; line-height: 180%; font-family: 仿宋_GB2312, 仿宋; font-size: 18px;"><p style="margin: 0px; text-indent: 0px;">正文</p><div style="background: url(http://my.hersp.com/ui/basic/smp_jp.gif) no-repeat center; padding: 60px; text-align: center; float: right;">海南省教育研究培训院<div>2016年3月3日</div></div></div>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'叙述式教学设计',
+        "html":'<h2 style="text-align: center;">课题名称</h2><h3 style="text-align: center;">作者</h3><p><strong>一、概述</strong></p><p>说明学科（数学、语言艺术等）和年级（中学、小学、学前等）;简要描述课题来源和所需课时；概述学习内容；概述这节课的价值以及学习内容的重要性。</p><p><strong>二、教学目标分析</strong></p><p>从知识与技能、过程与方法、情感态度与价值观三个维度对该课题预计达到的教学目标作出一个整体描述。</p><p><strong>三、学习者特征分析</strong></p><p>说明学习者在知识与技能、过程与方法、情感态度等三个方面的学习准备（学习起点），以及学生的学习风格。要注意结合特定的情景，切忌空泛。并说明教师是以何种方式进行的学习者特征分析，比如说是通过平时的观察、了解；或是通过预测题目的编制使用等。</p><p><strong>四、教学策略选择与设计</strong></p><p>说明本课题设计的基本理念，以及主要采用的教学与活动策略，以及这些策略实施过程中的关键问题。</p><p><strong>五、教学资源与工具设计</strong></p><p>一是为支持教师教的资源；二是支持学生学习的资源和工具，包括学习的环境、多媒体教学资源、特定的参考资料、参考网址、认知工具以及其它需要特别说明的传统媒体。如果是其它专题性学习、研究性学习方面的课程，可能还需要描述需要的人力支持及可获得情况。</p><p><strong>六、教学过程</strong></p><p>这一部分是该教学设计方案的关键所在。在这一部分，要说明教学的环节及所需的资源支持、具体的活动及其设计意图以及那些需要特别说明的教师引导语。最后，画出教学过程流程图。同时，流程图中需要清楚标注每一个阶段的教学目标、媒体和相应的评价方式。</p><p><strong>七、教学评价设计</strong></p><p>创建量规，向学生展示他们将如何被评价（来自教师和小组其他成员的评价）。另外，可以创建一个自我评价表，这样学生可以用它对自己的学习进行评价。</p><p><strong>八、帮助和总结</strong></p><p>说明教师以何种方式向学生提供帮助和指导，可以针对不同的学习阶段设计相应的不同帮助和指导，针对不同的学生提出不同水平的要求，给予不同的帮助。在学习结束后，对学生的学习做出简要总结。可以布置一些思考或练习题以强化学习效果，也可以提出一些问题或补充的链接鼓励学生超越这门课把思路拓展到其他内容领域。</p>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'主题式教学设计',
+        "html":'<table style="width: 100%;"><colgroup><col style="background: #f1f1f1;" /></colgroup><tbody><tr><td>姓名</td><td style="width: 80%; text-align: left;">&nbsp;</td></tr><tr><td>学校</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>电话</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>Email</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>主题活动选题</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>课程问题</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>活动设计摘要</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>学科领域</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>年级</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>学习目标/预期学习成果</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>国家课程标准</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>教学过程</td><td style="text-align: left;">&nbsp;</td></tr><tr><td>学习材料和资源</td><td style="text-align: left;">&nbsp;</td></tr></tbody></table>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'表格式教学设计1',
+        "html":'<table style="width: 100%;"><tbody><tr><td>案例名称</td><td colspan="3">&nbsp;</td><td>科目</td><td>&nbsp;</td></tr><tr><td>教学对象</td><td>&nbsp;</td><td>课时</td><td>&nbsp;</td><td>提供者</td><td>&nbsp;</td></tr><tr><td style="text-align: left;" colspan="6">一、教材内容分析</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">二、教学目标（知识与技能、过程与方法、情感态度与价值观）</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">三、学习者特征分析</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">四、教学策略选择与设计</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">五、教学环境及资源准备</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">六、教学过程</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">七、教学评价设计</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr><tr><td style="text-align: left;" colspan="6">八、帮助和总结</td></tr><tr><td style="text-align: left;" colspan="6"><br /><br /></td></tr></tbody></table>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'表格式教学设计2',
+        "html":'<table style="width: 100%;"><tbody><tr><td style="background: #f1f1f1;" colspan="2">基本信息</td></tr><tr><td>课题</td><td style="width: 68%;">&nbsp;</td></tr><tr><td>作者及工作单位</td><td>&nbsp;</td></tr><tr><td style="background: #f1f1f1;" colspan="2">教材分析</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">学情分析</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">教学目标</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">教学重点和难点</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">教学过程</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">板书设计</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">学生学习活动评价设计</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr><tr><td style="background: #f1f1f1;" colspan="2">教学反思</td></tr><tr><td style="text-align: left;" colspan="2"><br /><br /></td></tr></tbody></table>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'课堂实录',
+        "html":'<h2 style="text-align: center;">课题名称</h2><p>时间：</p><p>地点：</p><p>执教：</p><hr /><p>师：</p><p>生：</p>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'研修日志',
+        "html":'<h2 style="text-align: center;">xxxx 研修日志</h2><p>日期：</p><table style="width: 100%;"><tbody><tr><td style="width: 120px;">课题名称</td><td>&nbsp;</td><td style="width: 80px;">主讲</td><td>&nbsp;</td></tr><tr><td style="text-align: left;" colspan="4"><p>1.本专题最引起我关注的问题<br /><br /></p><p>2.本专题最能吸引我的观点<br /><br /></p><p>3.学习本专题后我的反思<br /><br /></p></td></tr><tr><td colspan="4">评价</td></tr><tr><td style="text-align: left;" colspan="4"><p>1.我对今天的培训活动感到：满意/一般/不满意<br /><br /></p><p>2.我认为今天的培训活动有待改进之处：<br /><br /></p></td></tr></tbody></table>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'教育叙事',
+        "html":'<h2 style="text-align: center;">标题</h2><h3 style="font-size: 14px; text-align: center;">作者<br />日期</h3><p><strong>案例概述</strong></p><p><br /><br /></p><p><strong>案例描述</strong></p><p><br /><br /></p><p><strong>反思感悟</strong></p><p><br /><br /></p>'
+
+    },{
+        "pre":"pre0.png",
+        'title':lang.blank,
+        'preHtml':'问题研讨',
+        "html":'<p><strong>研讨主题：</strong> &nbsp;</p><p><strong>研讨时间：</strong> &nbsp;</p><p><strong>主持人：</strong> &nbsp;</p><p><strong>研讨要求：</strong></p><p>1.</p><p>2.</p><p>注：每人限发一条评论</p><hr /><p><strong>结果整理：</strong></p><p>&nbsp;</p><p>&nbsp;</p>'
+    },
+    {
+        "pre":"pre1.png",
+        'title':lang.blog,
+        'preHtml':'<h1 label="Title center" name="tc" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;text-align:center;margin:0px 0px 20px;"><span style="color:#c0504d;">深入理解Range</span></h1><p style="text-align:center;"><strong class=" ">UEditor二次开发</strong></p><h3><span class=" " style="font-family:幼圆">什么是Range</span></h3><p style="text-indent:2em;">对于“插入”选项卡上的库，在设计时都充分考虑了其中的项与文档整体外观的协调性。 </p><br /><h3><span class=" " style="font-family:幼圆">Range能干什么</span></h3><p style="text-indent:2em;">在“开始”选项卡上，通过从快速样式库中为所选文本选择一种外观，您可以方便地更改文档中所选文本的格式。</p>',
+        "html":'<h1 class="ue_t" label="Title center" name="tc" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;text-align:center;margin:0px 0px 20px;"><span style="color:#c0504d;">[键入文档标题]</span></h1><p style="text-align:center;"><strong class="ue_t">[键入文档副标题]</strong></p><h3><span class="ue_t" style="font-family:幼圆">[标题 1]</span></h3><p class="ue_t"  style="text-indent:2em;">对于“插入”选项卡上的库，在设计时都充分考虑了其中的项与文档整体外观的协调性。 您可以使用这些库来插入表格、页眉、页脚、列表、封面以及其他文档构建基块。 您创建的图片、图表或关系图也将与当前的文档外观协调一致。</p><h3><span class="ue_t" style="font-family:幼圆">[标题 2]</span></h3><p class="ue_t"  style="text-indent:2em;">在“开始”选项卡上，通过从快速样式库中为所选文本选择一种外观，您可以方便地更改文档中所选文本的格式。 您还可以使用“开始”选项卡上的其他控件来直接设置文本格式。大多数控件都允许您选择是使用当前主题外观，还是使用某种直接指定的格式。 </p><h3><span class="ue_t" style="font-family:幼圆">[标题 3]</span></h3><p class="ue_t">对于“插入”选项卡上的库，在设计时都充分考虑了其中的项与文档整体外观的协调性。 您可以使用这些库来插入表格、页眉、页脚、列表、封面以及其他文档构建基块。 您创建的图片、图表或关系图也将与当前的文档外观协调一致。</p><p class="ue_t"><br /></p>'
+
+    },
+    {
+        "pre":"pre2.png",
+        'title':lang.resume,
+        'preHtml':'<h1 label="Title left" name="tl" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;margin:0px 0px 10px;"><span style="color:#e36c09;" class=" ">WEB前端开发简历</span></h1><table width="100%" border="1" bordercolor="#95B3D7" style="border-collapse:collapse;"><tbody><tr><td width="100" style="text-align:center;"><p><span style="background-color:transparent;">插</span><br /></p><p>入</p><p>照</p><p>片</p></td><td><p><span style="background-color:transparent;"> 联系电话：</span><span class="ue_t" style="background-color:transparent;">[键入您的电话]</span><br /></p><p><span style="background-color:transparent;"> 电子邮件：</span><span class="ue_t" style="background-color:transparent;">[键入您的电子邮件地址]</span><br /></p><p><span style="background-color:transparent;"> 家庭住址：</span><span class="ue_t" style="background-color:transparent;">[键入您的地址]</span><br /></p></td></tr></tbody></table><h3><span style="color:#E36C09;font-size:20px;">目标职位</span></h3><p style="text-indent:2em;" class=" ">WEB前端研发工程师</p><h3><span style="color:#e36c09;font-size:20px;">学历</span></h3><p><span style="display:none;line-height:0px;" id="_baidu_bookmark_start_26">﻿</span></p><ol style="list-style-type:decimal;"><li><p><span class="ue_t">[起止时间]</span> <span class="ue_t">[学校名称] </span> <span class="ue_t">[所学专业]</span> <span class="ue_t">[所获学位]</span></p></li></ol><h3><span style="color:#e36c09;font-size:20px;" class="ue_t">工作经验</span></h3><p><br /></p>',
+        "html":'<h1 label="Title left" name="tl" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;margin:0px 0px 10px;"><span style="color:#e36c09;" class="ue_t">[此处键入简历标题]</span></h1><p><span style="color:#e36c09;"><br /></span></p><table width="100%" border="1" bordercolor="#95B3D7" style="border-collapse:collapse;"><tbody><tr><td width="200" style="text-align:center;" class="ue_t">【此处插入照片】</td><td><p><br /></p><p> 联系电话：<span class="ue_t">[键入您的电话]</span></p><p><br /></p><p> 电子邮件：<span class="ue_t">[键入您的电子邮件地址]</span></p><p><br /></p><p> 家庭住址：<span class="ue_t">[键入您的地址]</span></p><p><br /></p></td></tr></tbody></table><h3><span style="color:#e36c09;font-size:20px;">目标职位</span></h3><p style="text-indent:2em;" class="ue_t">[此处键入您的期望职位]</p><h3><span style="color:#e36c09;font-size:20px;">学历</span></h3><p><span style="display:none;line-height:0px;" id="_baidu_bookmark_start_26">﻿</span></p><ol style="list-style-type:decimal;"><li><p><span class="ue_t">[键入起止时间]</span> <span class="ue_t">[键入学校名称] </span> <span class="ue_t">[键入所学专业]</span> <span class="ue_t">[键入所获学位]</span></p></li><li><p><span class="ue_t">[键入起止时间]</span> <span class="ue_t">[键入学校名称]</span> <span class="ue_t">[键入所学专业]</span> <span class="ue_t">[键入所获学位]</span></p></li></ol><h3><span style="color:#e36c09;font-size:20px;" class="ue_t">工作经验</span></h3><ol style="list-style-type:decimal;"><li><p><span class="ue_t">[键入起止时间]</span> <span class="ue_t">[键入公司名称]</span> <span class="ue_t">[键入职位名称]</span> </p></li><ol style="list-style-type:lower-alpha;"><li><p><span class="ue_t">[键入负责项目]</span> <span class="ue_t">[键入项目简介]</span></p></li><li><p><span class="ue_t">[键入负责项目]</span> <span class="ue_t">[键入项目简介]</span></p></li></ol><li><p><span class="ue_t">[键入起止时间]</span> <span class="ue_t">[键入公司名称]</span> <span class="ue_t">[键入职位名称]</span> </p></li><ol style="list-style-type:lower-alpha;"><li><p><span class="ue_t">[键入负责项目]</span> <span class="ue_t">[键入项目简介]</span></p></li></ol></ol><p><span style="color:#e36c09;font-size:20px;">掌握技能</span></p><p style="text-indent:2em;"> &nbsp;<span class="ue_t">[这里可以键入您所掌握的技能]</span><br /></p>'
+
+    },
+    {
+        "pre":"pre3.png",
+        'title':lang.richText,
+        'preHtml':'<h1 label="Title center" name="tc" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;text-align:center;margin:0px 0px 20px;" class="ue_t">[此处键入文章标题]</h1><p><img src="http://img.baidu.com/hi/youa/y_0034.gif" width="150" height="100" border="0" hspace="0" vspace="0" style="width:150px;height:100px;float:left;" />图文混排方法</p><p>图片居左，文字围绕图片排版</p><p>方法：在文字前面插入图片，设置居左对齐，然后即可在右边输入多行文</p><p><br /></p><p><img src="http://img.baidu.com/hi/youa/y_0040.gif" width="100" height="100" border="0" hspace="0" vspace="0" style="width:100px;height:100px;float:right;" /></p><p>还有没有什么其他的环绕方式呢？这里是居右环绕</p><p><br /></p><p>欢迎大家多多尝试，为UEditor提供更多高质量模板！</p>',
+        "html":'<p><br /></p><h1 label="Title center" name="tc" style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;text-align:center;margin:0px 0px 20px;" class="ue_t">[此处键入文章标题]</h1><p><img src="http://img.baidu.com/hi/youa/y_0034.gif" width="300" height="200" border="0" hspace="0" vspace="0" style="width:300px;height:200px;float:left;" />图文混排方法</p><p>1. 图片居左，文字围绕图片排版</p><p>方法：在文字前面插入图片，设置居左对齐，然后即可在右边输入多行文本</p><p><br /></p><p>2. 图片居右，文字围绕图片排版</p><p>方法：在文字前面插入图片，设置居右对齐，然后即可在左边输入多行文本</p><p><br /></p><p>3. 图片居中环绕排版</p><p>方法：亲，这个真心没有办法。。。</p><p><br /></p><p><br /></p><p><img src="http://img.baidu.com/hi/youa/y_0040.gif" width="300" height="300" border="0" hspace="0" vspace="0" style="width:300px;height:300px;float:right;" /></p><p>还有没有什么其他的环绕方式呢？这里是居右环绕</p><p><br /></p><p>欢迎大家多多尝试，为UEditor提供更多高质量模板！</p><p><br /></p><p>占位</p><p><br /></p><p>占位</p><p><br /></p><p>占位</p><p><br /></p><p>占位</p><p><br /></p><p>占位</p><p><br /></p><p><br /></p>'
+    },
+    {
+        "pre":"pre4.png",
+        'title':lang.sciPapers,
+        'preHtml':'<h2 style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;margin:0px 0px 10px;text-align:center;" class="ue_t">[键入文章标题]</h2><p><strong><span style="font-size:12px;">摘要</span></strong><span style="font-size:12px;" class="ue_t">：这里可以输入很长很长很长很长很长很长很长很长很差的摘要</span></p><p style="line-height:1.5em;"><strong>标题 1</strong></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">这里可以输入很多内容，可以图文混排，可以有列表等。</span></p><p style="line-height:1.5em;"><strong>标题 2</strong></p><ol style="list-style-type:lower-alpha;"><li><p class="ue_t">列表 1</p></li><li><p class="ue_t">列表 2</p></li><ol style="list-style-type:lower-roman;"><li><p class="ue_t">多级列表 1</p></li><li><p class="ue_t">多级列表 2</p></li></ol><li><p class="ue_t">列表 3<br /></p></li></ol><p style="line-height:1.5em;"><strong>标题 3</strong></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">来个文字图文混排的</span></p><p style="text-indent:2em;"><br /></p>',
+        'html':'<h2 style="border-bottom-color:#cccccc;border-bottom-width:2px;border-bottom-style:solid;padding:0px 4px 0px 0px;margin:0px 0px 10px;text-align:center;" class="ue_t">[键入文章标题]</h2><p><strong><span style="font-size:12px;">摘要</span></strong><span style="font-size:12px;" class="ue_t">：这里可以输入很长很长很长很长很长很长很长很长很差的摘要</span></p><p style="line-height:1.5em;"><strong>标题 1</strong></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">这里可以输入很多内容，可以图文混排，可以有列表等。</span></p><p style="line-height:1.5em;"><strong>标题 2</strong></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">来个列表瞅瞅：</span></p><ol style="list-style-type:lower-alpha;"><li><p class="ue_t">列表 1</p></li><li><p class="ue_t">列表 2</p></li><ol style="list-style-type:lower-roman;"><li><p class="ue_t">多级列表 1</p></li><li><p class="ue_t">多级列表 2</p></li></ol><li><p class="ue_t">列表 3<br /></p></li></ol><p style="line-height:1.5em;"><strong>标题 3</strong></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">来个文字图文混排的</span></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">这里可以多行</span></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">右边是图片</span></p><p style="text-indent:2em;"><span style="font-size:14px;" class="ue_t">绝对没有问题的，不信你也可以试试看</span></p><p><br /></p>'
+    }
+];
