4b468f33c6af5bc9681dd9b5ae3de303832e18b8
zhipeng.ma <zhipeng.ma@sihuatech.com>
2016-12-19 09:25:11 +0800

    栏目评论

---
+++ WebContent/db/cerhy_1_0_1_20161219.sql
@@ -0,0 +1,40 @@
+drop table `jc_comment_ext`
+drop table `jc_comment`;
+
+CREATE TABLE `jc_comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_user_id` int(11) DEFAULT NULL COMMENT '评论用户ID',
+  `reply_user_id` int(11) DEFAULT NULL COMMENT '回复用户ID',
+  `channel_id` int(11) DEFAULT NULL COMMENT '栏目ID',
+  `content_id` int(11) DEFAULT NULL COMMENT '内容ID',
+  `site_id` int(11) NOT NULL COMMENT '站点ID',
+  `create_time` datetime NOT NULL COMMENT '评论时间',
+  `reply_time` datetime DEFAULT NULL COMMENT '回复时间',
+  `ups` smallint(6) NOT NULL DEFAULT '0' COMMENT '支持数',
+  `downs` smallint(6) NOT NULL DEFAULT '0' COMMENT '反对数',
+  `is_recommend` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否推荐',
+  `is_checked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否审核',
+  `score` int(11) DEFAULT NULL COMMENT '评分',
+  `parent_id` int(11) DEFAULT NULL COMMENT '父级评论',
+  `reply_count` int(11) DEFAULT '0' COMMENT '回复数',
+  PRIMARY KEY (`comment_id`),
+  KEY `fk_jc_comment_channel` (`channel_id`),
+  KEY `fk_jc_comment_content` (`content_id`),
+  KEY `fk_jc_comment_reply` (`reply_user_id`),
+  KEY `fk_jc_comment_site` (`site_id`),
+  KEY `fk_jc_comment_user` (`comment_user_id`),
+  CONSTRAINT `fk_jc_comment_channel` FOREIGN KEY (`channel_id`) REFERENCES `jc_channel` (`channel_id`),
+  CONSTRAINT `fk_jc_comment_content` FOREIGN KEY (`content_id`) REFERENCES `jc_content` (`content_id`),
+  CONSTRAINT `fk_jc_comment_reply` FOREIGN KEY (`reply_user_id`) REFERENCES `jc_user` (`user_id`),
+  CONSTRAINT `fk_jc_comment_site` FOREIGN KEY (`site_id`) REFERENCES `jc_site` (`site_id`),
+  CONSTRAINT `fk_jc_comment_user` FOREIGN KEY (`comment_user_id`) REFERENCES `jc_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8 COMMENT='CMS评论表';
+
+CREATE TABLE `jc_comment_ext` (
+  `comment_id` int(11) NOT NULL,
+  `ip` varchar(50) DEFAULT NULL COMMENT 'IP地址',
+  `text` longtext COMMENT '评论内容',
+  `reply` longtext COMMENT '回复内容',
+  KEY `fk_jc_ext_comment` (`comment_id`),
+  CONSTRAINT `fk_jc_ext_comment` FOREIGN KEY (`comment_id`) REFERENCES `jc_comment` (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='CMS评论扩展表';
