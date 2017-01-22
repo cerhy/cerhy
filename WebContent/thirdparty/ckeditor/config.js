605107cf500a997170bcb523d441578e367b5299
zph <895464868@qq.com>
2017-01-22 17:24:10 +0800

    1

--- WebContent/thirdparty/ckeditor/config.js
+++ WebContent/thirdparty/ckeditor/config.js
@@ -44,6 +44,11 @@ CKEDITOR.editorConfig = function( config ) {
       		['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
       	];
 	    config.extraPlugins = 'autoformat,page';
+	    config.font_names = '隶书/隶书;宋体/SimSun;新宋体/NSimSun;黑体/SimHei;楷体/KaiTi;微软雅黑/Microsoft YaHei;'+  
+        '楷体_GB2312/KaiTi_GB2312;仿宋/FangSong;仿宋_GB2312/FangSong_GB2312;华文细黑/STXihei;华文楷体/STKaiti;华文宋体/STSong;华文中宋/STZhongsong;'+  
+        '华文仿宋/STFangsong;华文彩云/STCaiyun;华文琥珀/STHupo;华文隶书/STLiti;华文行楷/STXingkai;华文新魏/STXinwei;'+  
+        '方正舒体/FZShuTi;方正姚体/FZYaoti;细明体/MingLiU;新细明体/PMingLiU;微软正黑/Microsoft JhengHei;'+  
+        'Arial Black/Arial Black;'+ config.font_names;
 };
 
 CKEDITOR.on( 'instanceReady', function( ev ) { with (ev.editor.dataProcessor.writer) {
