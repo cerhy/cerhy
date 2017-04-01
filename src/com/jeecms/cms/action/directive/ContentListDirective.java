f308830b7dfd0f9049e7b83a8a352e9a60771489
zph <895464868@qq.com>
2017-04-01 09:14:41 +0800

    性能优化

--- src/com/jeecms/cms/action/directive/ContentListDirective.java
+++ src/com/jeecms/cms/action/directive/ContentListDirective.java
@@ -7,18 +7,24 @@ import static com.jeecms.common.web.freemarker.DirectiveUtils.OUT_LIST;
 import static com.jeecms.core.web.util.FrontUtils.PARAM_STYLE_LIST;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import redis.clients.jedis.Jedis;
 
 import com.jeecms.cms.action.directive.abs.AbstractContentDirective;
 import com.jeecms.cms.entity.main.Content;
+import com.jeecms.common.util.RedisUtil;
 import com.jeecms.common.web.freemarker.DefaultObjectWrapperBuilderFactory;
 import com.jeecms.common.web.freemarker.DirectiveUtils;
-import com.jeecms.common.web.freemarker.ParamsRequiredException;
 import com.jeecms.common.web.freemarker.DirectiveUtils.InvokeType;
+import com.jeecms.common.web.freemarker.ParamsRequiredException;
 import com.jeecms.core.entity.CmsSite;
 import com.jeecms.core.web.util.FrontUtils;
 
@@ -31,6 +37,7 @@ import freemarker.template.TemplateModel;
  * 内容列表标签
  */
 public class ContentListDirective extends AbstractContentDirective {
+	private static final Logger logger = Logger.getLogger(ContentListDirective.class);
 	/**
 	 * 模板名称
 	 */
@@ -45,33 +52,55 @@ public class ContentListDirective extends AbstractContentDirective {
 	public void execute(Environment env, Map params, TemplateModel[] loopVars,
 			TemplateDirectiveBody body) throws TemplateException, IOException {
 		CmsSite site = FrontUtils.getSite(env);
-		List<Content> list = getList(params, env);
-
+		String channelIds=params.get("channelId").toString();
+		List<Content> list=new ArrayList<Content>();
+		String key = channelIds;  
+		try {
+			list=RedisUtil.getList(key);
+		} catch (Exception e) {
+			list = getList(params, env);
+			logger.error("redis读取异常,切回数据库读取", e);
+		}
+//		System.out.println("开始时间1:"+new Date());
+		if(list==null||list.size()<=0){
+			RedisUtil.setList(key, list);
+		}
+//		System.out.println("结束时间2:"+new Date());
 		Map<String, TemplateModel> paramWrap = new HashMap<String, TemplateModel>(
 				params);
 		paramWrap.put(OUT_LIST, DefaultObjectWrapperBuilderFactory.getDefaultObjectWrapper().wrap(list));
 		Map<String, TemplateModel> origMap = DirectiveUtils
 				.addParamsToVariable(env, paramWrap);
+//		System.out.println("结束时间3:"+new Date());
 		InvokeType type = DirectiveUtils.getInvokeType(params);
 		String listStyle = DirectiveUtils.getString(PARAM_STYLE_LIST, params);
-		if (InvokeType.sysDefined == type) {
-			if (StringUtils.isBlank(listStyle)) {
-				throw new ParamsRequiredException(PARAM_STYLE_LIST);
+		try {
+			if (InvokeType.sysDefined == type) {
+				if (StringUtils.isBlank(listStyle)) {
+					throw new ParamsRequiredException(PARAM_STYLE_LIST);
+				}
+				env.include(TPL_STYLE_LIST + listStyle + TPL_SUFFIX, UTF8, true);
+			} else if (InvokeType.userDefined == type) {
+				if (StringUtils.isBlank(listStyle)) {
+					throw new ParamsRequiredException(PARAM_STYLE_LIST);
+				}
+				FrontUtils.includeTpl(TPL_STYLE_LIST, site, env);
+			} else if (InvokeType.custom == type) {
+				FrontUtils.includeTpl(TPL_NAME, site, params, env);
+			} else if (InvokeType.body == type) {
+//				System.out.println("结束时间4:"+new Date());
+				body.render(env.getOut());
+//				System.out.println("结束时间5:"+new Date());
+			} else {
+				throw new RuntimeException("invoke type not handled: " + type);
 			}
-			env.include(TPL_STYLE_LIST + listStyle + TPL_SUFFIX, UTF8, true);
-		} else if (InvokeType.userDefined == type) {
-			if (StringUtils.isBlank(listStyle)) {
-				throw new ParamsRequiredException(PARAM_STYLE_LIST);
-			}
-			FrontUtils.includeTpl(TPL_STYLE_LIST, site, env);
-		} else if (InvokeType.custom == type) {
-			FrontUtils.includeTpl(TPL_NAME, site, params, env);
-		} else if (InvokeType.body == type) {
-			body.render(env.getOut());
-		} else {
-			throw new RuntimeException("invoke type not handled: " + type);
+		} catch (Exception e) {
+//			System.out.println("---------"+params.get("channelId").toString());
+			e.printStackTrace();
 		}
 		DirectiveUtils.removeParamsFromVariable(env, paramWrap, origMap);
+//		System.out.println("结束时间6:"+new Date());
+//		System.out.println("结束");
 	}
 
 	@SuppressWarnings("unchecked")
@@ -81,7 +110,10 @@ public class ContentListDirective extends AbstractContentDirective {
 		if (ids != null) {
 			return contentMng.getListByIdsForTag(ids, getOrderBy(params));
 		} else {
-			return (List<Content>) super.getData(params, env);
+			System.out.println("时间st:"+new Date());
+			List<Content> list=(List<Content>) super.getData(params, env);
+			System.out.println("时间en:"+new Date());
+			return list;
 		}
 	}
 
---
+++ src/com/jeecms/common/util/RedisUtil.java
@@ -0,0 +1,247 @@
+package com.jeecms.common.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.spy.memcached.compat.CloseUtil;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.jeecms.cms.entity.main.Content;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public final class RedisUtil {
+	private static final Logger logger = Logger
+			.getLogger(RedisUtil.class);
+	private static JedisPool jedisPool = null;
+
+	/**
+	 * 初始化Redis连接池
+	 */
+	static {
+		try {
+			WebApplicationContext wac = ContextLoader
+					.getCurrentWebApplicationContext();
+			jedisPool = (JedisPool) wac.getBean("jedisPool");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取Jedis实例
+	 * 
+	 * @return
+	 */
+	public synchronized static Jedis getJedis() {
+		Jedis resource = null;
+		//int count=0;
+	
+			try {
+				if (jedisPool != null) {
+					resource = jedisPool.getResource();
+					return resource;
+				} else {
+					return null;
+				}
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+				logger.info("redis连接异常，切换回数据库读取");
+				return null;
+			}
+	      
+	}
+
+	/**
+	 * 释放jedis资源
+	 * 
+	 * @param jedis
+	 */
+	public static void returnResource(final Jedis jedis) {
+		if (jedis != null) {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	/**
+	 * 根据 className,vin得到对应redis实体
+	 * 
+	 * @param jedis
+	 */
+	public static List<Content> getList(String key) {
+		List<Content> list = null;
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			if(jedis!=null){
+				 byte[] in = jedis.get(key.getBytes());  
+			        list = (List<Content>)ListTranscoder.deserialize(in);  
+			}else{
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnResource(jedis);
+		} finally {
+			returnResource(jedis);
+		}
+
+		return list;
+	}
+	public static void setList(String key,List<Content> list) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			if(jedis!=null){
+				jedis.set(key.getBytes(), ListTranscoder.serialize(list));  
+			}
+		} catch (Exception e) {
+			logger.error("在存储时候抛出异常",e); 
+			returnResource(jedis);
+		} finally {
+			returnResource(jedis);
+		}
+		
+	}
+	
+	
+	public static void close(Closeable closeable) {  
+        if (closeable != null) {  
+            try {  
+                closeable.close();  
+            } catch (Exception e) {  
+                logger.info("Unable to close %s",e);  
+            }  
+        }  
+    }  
+  
+    static class User implements Serializable{  
+        String name;  
+  
+        public String getName() {  
+            return name;  
+        }  
+  
+        public void setName(String name) {  
+            this.name = name;  
+        }  
+    }  
+  
+    static class ObjectsTranscoder{  
+          
+        public static byte[] serialize(List<User> value) {  
+            if (value == null) {  
+                throw new NullPointerException("Can't serialize null");  
+            }  
+            byte[] rv=null;  
+            ByteArrayOutputStream bos = null;  
+            ObjectOutputStream os = null;  
+            try {  
+                bos = new ByteArrayOutputStream();  
+                os = new ObjectOutputStream(bos);  
+                for(User user : value){  
+                    os.writeObject(user);  
+                }  
+                os.writeObject(null);  
+                os.close();  
+                bos.close();  
+                rv = bos.toByteArray();  
+            } catch (IOException e) {  
+                throw new IllegalArgumentException("Non-serializable object", e);  
+            } finally {  
+                close(os);  
+                close(bos);  
+            }  
+            return rv;  
+        }  
+  
+        public static List<User> deserialize(byte[] in) {  
+            List<User> list = new ArrayList<User>();  
+            ByteArrayInputStream bis = null;  
+            ObjectInputStream is = null;  
+            try {  
+                if(in != null) {  
+                    bis=new ByteArrayInputStream(in);  
+                    is=new ObjectInputStream(bis);  
+                    while (true) {  
+                        User user = (User) is.readObject();  
+                        if(user == null){  
+                            break;  
+                        }else{  
+                            list.add(user);  
+                        }  
+                    }  
+                    is.close();  
+                    bis.close();  
+                }  
+            } catch (IOException e) {  
+                logger.error("Caught IOException decoding %d bytes of data",e);  
+            } catch (ClassNotFoundException e) {  
+                logger.error("Caught CNFE decoding %d bytes of data",e);  
+            } finally {  
+                CloseUtil.close(is);  
+                CloseUtil.close(bis);  
+            }  
+            return list;  
+        }  
+    }  
+      
+    static class ListTranscoder{  
+        public static byte[] serialize(Object value) {  
+            if (value == null) {  
+                throw new NullPointerException("Can't serialize null");  
+            }  
+            byte[] rv=null;  
+            ByteArrayOutputStream bos = null;  
+            ObjectOutputStream os = null;  
+            try {  
+                bos = new ByteArrayOutputStream();  
+                os = new ObjectOutputStream(bos);  
+                os.writeObject(value);  
+                os.close();  
+                bos.close();  
+                rv = bos.toByteArray();  
+            } catch (IOException e) {  
+                throw new IllegalArgumentException("Non-serializable object", e);  
+            } finally {  
+                close(os);  
+                close(bos);  
+            }  
+            return rv;  
+        }  
+  
+        public static Object deserialize(byte[] in) {  
+            Object rv=null;  
+            ByteArrayInputStream bis = null;  
+            ObjectInputStream is = null;  
+            try {  
+                if(in != null) {  
+                    bis=new ByteArrayInputStream(in);  
+                    is=new ObjectInputStream(bis);  
+                    rv=is.readObject();  
+                    is.close();  
+                    bis.close();  
+                }  
+            } catch (IOException e) {  
+                logger.error("Caught IOException decoding %d bytes of data", e);
+            } catch (ClassNotFoundException e) {  
+                logger.error("Caught CNFE decoding %d bytes of data",e);  
+            } finally {  
+                CloseUtil.close(is);  
+                CloseUtil.close(bis);  
+            }  
+            return rv;  
+        }  
+    }  
+}  
+	
