f11912fb8d99a215899d6cabaeeae599a5bfa3b6
zph <895464868@qq.com>
2017-05-15 18:13:20 +0800

    1

--- WebContent/db/zhengpeihui20170509.sql
+++ WebContent/db/zhengpeihui20170509.sql
@@ -1,3 +1,4 @@
 alter table `jc_user` add column `areacode` int(2) DEFAULT NULL COMMENT '手机国家地区代码';
 alter table `jc_user` add column `type` int(2) DEFAULT NULL COMMENT '用户类型:0:教师,1:学生,2:教研工作人员,3:家长,4:其他';
+alter table `jc_user` add column `nickname` varchar(50) DEFAULT NULL COMMENT '昵称';
 
