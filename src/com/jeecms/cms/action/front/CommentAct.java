306d305114d7384be723620b6db867c530abe46f
zph <895464868@qq.com>
2017-08-08 20:14:52 +0800

    微信分享跳转单独分享页面和老博客保持一致

--- src/com/jeecms/cms/action/front/CommentAct.java
+++ src/com/jeecms/cms/action/front/CommentAct.java
@@ -47,6 +47,7 @@ public class CommentAct {
 
 	public static final String COMMENT_PAGE = "tpl.commentPage";
 	public static final String COMMENT_LIST = "tpl.commentList";
+	public static final String COMMENT_LIST_SHARE = "tpl.commentListShare";
 	public static final String COMMENT_INPUT = "tpl.commentInput";
 	public static final String COMMENT_CHANNEL_INPUT = "tpl.commentChannelInput";
 	public static final String COMMENT_CHANNEL_PAGE = "tpl.commentChannelPage";
@@ -213,6 +214,45 @@ public class CommentAct {
 		
 		return FrontUtils.getTplPath(request, site.getSolutionPath(),
 				TPLDIR_CSI, COMMENT_LIST);
+	}
+	@RequestMapping(value = "/comment_list_share.jspx")
+	public String comment_list_share(Integer siteId,Integer contentId, Integer parentId,
+			Integer greatTo,Integer recommend, Integer checked, 
+			Integer orderBy, Integer count,
+			HttpServletRequest request, HttpServletResponse response,
+			ModelMap model) {
+		if (count == null || count <= 0 || count > 200) {
+			count = 200;
+		}
+		boolean desc;
+		if (orderBy == null || orderBy == 0) {
+			desc = true;
+		} else {
+			desc = false;
+		}
+		Boolean rec;
+		if (recommend != null) {
+			rec = recommend != 0;
+		} else {
+			rec = null;
+		}
+		Boolean chk;
+		if (checked != null) {
+			chk = checked != 0;
+		} else {
+			chk = null;
+		}
+		List<CmsComment> list = cmsCommentMng.getListForTag(siteId,contentId,
+				parentId,greatTo, chk, rec, desc, count);
+		// 将request中所有参数
+		model.putAll(RequestUtils.getQueryParams(request));
+		model.addAttribute("list", list);
+		model.addAttribute("contentId", contentId);
+		CmsSite site = CmsUtils.getSite(request);
+		FrontUtils.frontData(request, model, site);
+		
+		return FrontUtils.getTplPath(request, site.getSolutionPath(),
+				TPLDIR_CSI, COMMENT_LIST_SHARE);
 	}
 
 	/**
