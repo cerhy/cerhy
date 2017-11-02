4add8c46066f5756edf0021e086f180e6a385edf
lihao <670305450@qq.com>
2017-11-02 15:53:08 +0800

    评论内容英文括号乱码问题

--- src/com/jeecms/cms/action/front/CommentAct.java
+++ src/com/jeecms/cms/action/front/CommentAct.java
@@ -465,6 +465,12 @@ public class CommentAct {
 		CmsSite site = CmsUtils.getSite(request);
 		CmsUser user = CmsUtils.getUser(request);
 		JSONObject json = new JSONObject();
+		if(text.contains("&＃40;")){
+			text=text.replace("&＃40;", "(");
+		}
+		if(text.contains("&＃41;")){
+			text=text.replace("&＃41;", ")");
+		}
 		if (contentId == null) {
 			json.put("success", false);
 			json.put("status", 100);
