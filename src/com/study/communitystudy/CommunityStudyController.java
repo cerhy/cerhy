215816fece62e420e5491aa7efc5de43f180b2e4
liufei <331036525@qq.com>
2018-02-24 16:50:27 +0800

    代码整理

--- src/com/study/communitystudy/CommunityStudyController.java
+++ src/com/study/communitystudy/CommunityStudyController.java
@@ -6,23 +6,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.jeecms.cms.action.project.CommunityAct;
 
 /**
  * 社区研修
  */
 @Controller
+@RequestMapping("/CommunityStudy")    
 public class CommunityStudyController {
 	@SuppressWarnings("unused")
 	
 	private static final Logger log = LoggerFactory.getLogger(CommunityStudyController.class);
 	
-    @RequestMapping("/cte")    
+    @RequestMapping("/index")    
     public ModelAndView index(HttpServletRequest request,HttpServletResponse response) {    
     	System.out.println("++++++++++++++++++++++++++++++++++++++");
         return new ModelAndView("WEB-INF/study/community.html");    
